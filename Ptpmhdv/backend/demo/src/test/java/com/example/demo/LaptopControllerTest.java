7c36f9aecd9ea45dcfc7b8bb43893920d060aa0e
Phanthai1601 <phanhoangthai765@gmail.com>
2025-06-04 23:49:51 +0700

    add github actions

---
+++ Ptpmhdv/backend/demo/src/test/java/com/example/demo/LaptopControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LaptopControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetLaptopById_found() throws Exception {
+        mockMvc.perform(get("/api/laptops/16")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.name").exists());
+    }
+
+    @Test
+    public void testGetLaptopById_notFound() throws Exception {
+        mockMvc.perform(get("/api/laptops/999")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
